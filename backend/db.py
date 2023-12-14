b27cdf60fe1641b9f82f2c25a9c5ebe9fe35b5c6
Liam-Tomas <ltarmstrong94@gmail.com>
2023-12-14 15:29:36 -0800

    moved requirements.txt into backend dir. for use w/ render

--- backend/db.py
+++ backend/db.py
@@ -15,10 +15,11 @@ import os
 
 def get_db_connection():
     conn = mysql.connector.connect(
-        host=os.getenv('DB_HOST', 'localhost'),
-        user=os.getenv('DB_USER', 'root'),
+        host=os.getenv('DB_HOST'),
+        user=os.getenv('DB_USER'),
         password=os.getenv('DB_PASSWORD'),
-        database=os.getenv('DB_NAME', 'sfpd_incidents_db'),
-        port=int(os.getenv('DB_PORT', 3306))
+        database=os.getenv('DB_NAME'),
+        port=int(os.getenv('DB_PORT'))  # Assuming PlanetScale provides a port
     )
     return conn
+
